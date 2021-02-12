b5f79978bfd230cb6f4760878299f99d4182de24
PovilasKorop <povilask007@gmail.com>
2021-02-12 07:54:38 +0200

    Models and migrations: first version

---
+++ app/Models/Review.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['book_id', 'user_id', 'stars', 'comment'];
+}
---
+++ database/migrations/2021_02_12_042345_create_book_genre_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookGenreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_genre', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained();
+            $table->foreignId('genre_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_genre');
+    }
+}
